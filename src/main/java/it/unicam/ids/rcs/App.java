3ecdd59220e40696576615db077fbe3251511e0f
Simone Governatori <simone.governatori@studenti.unicam.it>
2026-01-17 12:24:57 +0100

    Aggiunta licenza corretta e documentazione metodi

--- src/main/java/it/unicam/ids/rcs/App.java
+++ src/main/java/it/unicam/ids/rcs/App.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2025 Simone Governatori
+ * Copyright (c) 2026 Riccardo Farina, Cristian Bellesi, Simone Governatori
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- src/main/java/it/unicam/ids/rcs/handler/CreaHackatonHandler.java
+++ src/main/java/it/unicam/ids/rcs/handler/CreaHackatonHandler.java
@@ -1,3 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Riccardo Farina, Cristian Bellesi, Simone Governatori
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package it.unicam.ids.rcs.handler;
 
 import it.unicam.ids.rcs.controller.HackatonController;
@@ -7,7 +32,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
- *
+ * Questa classe rappresenta un gestore del caso d'uso Crea Hackaton.
+ * Espone le funzioni necessarie ad avviare, gestire e completare la registrazione
+ * di un nuovo hackaton all'interno del sistema.
  */
 public class CreaHackatonHandler {
 
@@ -20,15 +47,15 @@ public class CreaHackatonHandler {
     }
 
     /**
-     *
-     * @param dimensioneMassimaTeam
-     * @param regolamento
-     * @param scadenzaIscrizioni
-     * @param inizio
-     * @param fine
-     * @param luogo
-     * @param premio
-     * @return
+     * Avvia il processo di creazione dell'hackaton
+     * @param dimensioneMassimaTeam La dimensione massima di ciascun team
+     * @param regolamento Il regolamento completo dell'hackaton
+     * @param scadenzaIscrizioni La data di scadenza delle iscrizioni
+     * @param inizio Data di inizio dell'hackaton
+     * @param fine Data di fine dell'hackaton
+     * @param luogo Il luogo in cui si svolge l'hackaton
+     * @param premio Il premio in denaro per il vincitore dell'hackaton
+     * @return <code>True</code> se l'operazione termina con successo, <code>false</code> altrimenti
      */
     public boolean creaHackaton(int dimensioneMassimaTeam, String regolamento, LocalDate scadenzaIscrizioni,
                              LocalDateTime inizio, LocalDateTime fine, String luogo, Double premio)
@@ -39,27 +66,31 @@ public class CreaHackatonHandler {
     }
 
     /**
-     *
-     * @param email
-     * @return
+     * Designa l'utente indicato attraverso l'e-mail come giudice dell'hackaton
+     * @param email L'e-mail dell'utente da designare come giudice
+     * @return <code>True</code> in caso di successo, <code>false</code> altrimenti
+     * (e.g. Non esistono utenti con l'e-mail fornita)
      */
     public boolean assegnaGiudice(String email){
         return email != null && !email.isEmpty() && this.hackatonController.assegnaGiudice(email);
     }
 
     /**
-     *
-     * @param email
-     * @return
+     * Designa l'utente indicato attraverso l'e-mail come mentore dell'hackaton
+     * @param email L'e-mail dell'utente da designare come mentore
+     * @return <code>True</code> in caso di successo, <code>false</code> altrimenti
+     * (e.g. Non esistono utenti con l'e-mail fornita)
      */
     public boolean aggiungiMentore(String email){
         return email != null && !email.isEmpty() && this.hackatonController.aggiungiMentore(email);
     }
 
     /**
-     *
-     * @param emailCreatore
-     * @return
+     * Conferma la creazione dell'hackaton. L'utente relativo all'e-mail fornita
+     * viene designato come organizzatore. Il sistema registra l'hackaton
+     * @param emailCreatore L'e-mail dell'utente che sta creando l'hackaton e che
+     *                      ne diventerà organizzatore
+     * @return <code>Hackaton</code> in caso di successo, <code>null</code> altrimenti
      */
     public Hackaton confermaCreazione(String emailCreatore){
         return this.hackatonController.registraHackaton(emailCreatore);
