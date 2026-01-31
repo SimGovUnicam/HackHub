55def27d490ba9c88bc14e9bba1f7f97915ded0a
Riccardo <riccardo.farina@studenti.unicam.it>
2026-01-31 10:53:30 +0100

    Estensione della classe notifca

--- src/main/java/it/unicam/ids/rcs/model/Notifica.java
+++ src/main/java/it/unicam/ids/rcs/model/Notifica.java
@@ -1,28 +1,39 @@
 
 package it.unicam.ids.rcs.model;
 
-public class Notifica {
+public abstract class Notifica
+{
     private Utente mittente;
     private Utente destinatario;
-    private String messagiio;
-}
-public Notifica(Utente mittente, Utente destinatario, String messaggio){
-    this.mittente= mittente;
-    this.destinatario= destinatario;
-    this.messaggio= messaggio;
-}
-public String ottieniMessaggioPerOrganizzatore;
-public String ottieniMessaggioPerGiudice;
-public String ottieniMessaggioPerMentore;
-public String ottieniMessaggioPerMembroDelloStaff;
-public String ottieniMessaggioPerMemebroDelTeam;
+    private String messaggio;
 
-public Utente getMittente(){
-    return mittente;
+    public Notifica(Utente mittente, Utente destinatario, String messaggio)
+    {
+        this.mittente= mittente;
+        this.destinatario= destinatario;
+        this.messaggio= messaggio;
+    }
+
+    public abstract String ottieniMessaggioPerOrganizzatore();
+    public abstract String ottieniMessaggioPerGiudice();
+    public abstract String ottieniMessaggioPerMentore();
+    public abstract String ottieniMessaggioPerMembroDelloStaff();
+    public abstract String ottieniMessaggioPerMemebroDelTeam();
+
+    public Utente getMittente()
+    {
+     return this.mittente=mittente;
+    }
+    public Utente getDestinatario()
+    {
+        return destinatario;
+    }
+    public String getMessaggio()
+    {
+        return messaggio;
+    }
+
 }
-public Utente getDestinatario(){
-    return destinatario;
-}
-public String getMessaggio(){
-    return messaggio;
-}
+
+
+
---
+++ src/main/java/it/unicam/ids/rcs/model/NotificaCreazioneHackaton.java
@@ -0,0 +1,31 @@
+package it.unicam.ids.rcs.model;
+
+public class NotificaCreazioneHackaton extends Notifica{
+    private Hackaton hackaton;
+
+        public NotificaCreazioneHackaton(Hackaton hackaton, Utente utente){
+        super(utente,null,"");
+        this.hackaton=hackaton;
+
+        }
+        @Override
+        public String ottieniMessaggioPerOrganizzatore(){
+            return "É stato creato un nuovo Hackaton: "+ hackaton.getNome();
+        }
+        @Override
+        public String ottieniMessaggioPerGiudice(){
+            return "É stato creato un nuovo Hackaton: "+ hackaton.getNome();
+        }
+        @Override
+        public String ottieniMessaggioPerMentore(){
+            return "É stato creato un nuovo Hackaton: "+ hackaton.getNome();
+        }
+        @Override
+        public String ottieniMessaggioPerMembroDelloStaff(){
+            return "É stato creato un nuovo Hackaton: "+ hackaton.getNome();
+        }
+        @Override
+        public String ottieniMessaggioPerMemebroDelTeam(){
+            return "É stato creato un nuovo Hackaton: "+ hackaton.getNome();
+        }
+}
