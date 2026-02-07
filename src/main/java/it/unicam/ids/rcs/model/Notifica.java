035c7f6720413367553cd286e6bc72161a884a9a
Riccardo <riccardo.farina@studenti.unicam.it>
2026-02-07 10:58:39 +0100

    Aggiunta licensa Copyright al codice e aggiunta setter e getter alla classe Notifica

--- src/main/java/it/unicam/ids/rcs/model/Notifica.java
+++ src/main/java/it/unicam/ids/rcs/model/Notifica.java
@@ -1,6 +1,33 @@
 
-package it.unicam.ids.rcs.model;
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Riccardo Farina, Cristian Bellesi, Simone Governatori
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 
+package it.unicam.ids.rcs.model;
+/*
+Questa classe viene usata per la gestione delle notifiche  
+ */
 public abstract class Notifica {
     private Utente mittente;
     private Utente destinatario;
@@ -14,6 +41,31 @@ public abstract class Notifica {
     protected Notifica() {
     }
 
+    public Utente getMittente() {
+        return mittente;
+    }
+
+    public Utente getDestinatario() {
+        return destinatario;
+    }
+
+    public String getMessaggio() {
+        return messaggio;
+    }
+
+    private void setMittente(Utente mittente) {
+        this.mittente = mittente;
+    }
+
+    private void setDestinatario(Utente destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    protected String setMessaggio(String messaggio) {
+        this.messaggio = messaggio;
+        return messaggio;
+    }
+
     public abstract String ottieniMessaggioPerOrganizzatore();
 
     public abstract String ottieniMessaggioPerGiudice();
@@ -23,6 +75,8 @@ public abstract class Notifica {
     public abstract String ottieniMessaggioPerMembroDelloStaff();
 
     public abstract String ottieniMessaggioPerMembroDelTeam();
+
+    public abstract String ottieniMessaggioPerUtente();
 }
 
 
