faadabcfcaf771524136d500088c21ad69d47f2d
Simone Governatori <simone.governatori@studenti.unicam.it>
2026-02-19 14:52:50 +0100

    Aggiunte dipendenze SpringBoot ed entry point del sistema

--- src/main/java/it/unicam/ids/rcs/App.java
+++ src/main/java/it/unicam/ids/rcs/App.java
@@ -25,11 +25,15 @@
 
 package it.unicam.ids.rcs;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * System entry point
  */
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-
+        SpringApplication.run(App.class, args);
     }
 }
